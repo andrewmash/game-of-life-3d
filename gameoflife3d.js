ccdb83b2225c5586e2c00edbc23f11b5feaa01bc
Andrew Ash <andrewash9@gmail.com>
2016-02-04 20:38:18 -0500

    beginning to add 3d visualization

--- gameoflife3d.js
+++ gameoflife3d.js
@@ -1,72 +1,82 @@
-var worldSize = 30;
-var cells = seed();
+var game = (function() {
 
-function seed() {
+	var worldSize = 30;
 	var world = [];
-	var column = [];
-	var aisle = [];
-	for (var i = 0; i < worldSize; i++) {
-		for (var j = 0; j < worldSize; j++) {
-			for (var k = 0; k < worldSize; k++) {
-				if (Math.random() < 0.2) aisle.push(1);
-				else aisle.push(0);
-			}
-			column.push(aisle);
-			aisle = [];
-		}
-		world.push(column);
-		column = [];
-	}
-	return world;
-}
 
-function nextGen(cells) {
-	var newGen = [];
-	var newColumn = [];
-	var newAisle = [];
-	cells.forEach(function(row, rowIndex) {
-		row.forEach(function(column, columnIndex) {
-			column.forEach(function(cell, aisleIndex) {
-				var population = getPop(cells, rowIndex, columnIndex, aisleIndex, cell);
-				newAisle.push(isItAlive(population, cell));
+	function seed() {
+		var column = [];
+		var aisle = [];
+		for (var i = 0; i < worldSize; i++) {
+			for (var j = 0; j < worldSize; j++) {
+				for (var k = 0; k < worldSize; k++) {
+					if (Math.random() < 0.2) aisle.push(1);
+					else aisle.push(0);
+				}
+				column.push(aisle);
+				aisle = [];
+			}
+			world.push(column);
+			column = [];
+		}
+		return world;
+	}
+
+	function nextGen() {
+		var newGen = [];
+		var newColumn = [];
+		var newAisle = [];
+		world.forEach(function(row, rowIndex) {
+			row.forEach(function(column, columnIndex) {
+				column.forEach(function(cell, aisleIndex) {
+					var population = getPop(world, rowIndex, columnIndex, aisleIndex, cell);
+					newAisle.push(isItAlive(population, cell));
+				});
+				newColumn.push(newAisle);
+				newAisle = [];
 			});
-			newColumn.push(newAisle);
-			newAisle = [];
+			newGen.push(newColumn);
+			newColumn = [];
 		});
-		newGen.push(newColumn);
-		newColumn = [];
-	});
-	return newGen;
-}
+		world = newGen;
+	}
 
-function getPop(cells, rowIndex, columnIndex, aisleIndex, cell) {
-	var population = 0;
-	for (var i = rowIndex - 1; i <= rowIndex + 1; i++) {
-		for (var j = columnIndex - 1; j <= columnIndex + 1; j++) {
-			for (var k = aisleIndex - 1; k <= aisleIndex + 1; k++) {
-				if (cells[wrap(i)][wrap(j)][wrap(k)] === 1) population++;
+	function getPop(world, rowIndex, columnIndex, aisleIndex, cell) {
+		var population = 0;
+		for (var i = rowIndex - 1; i <= rowIndex + 1; i++) {
+			for (var j = columnIndex - 1; j <= columnIndex + 1; j++) {
+				for (var k = aisleIndex - 1; k <= aisleIndex + 1; k++) {
+					if (world[wrap(i)][wrap(j)][wrap(k)] === 1) population++;
+				}
 			}
 		}
+		return population;
 	}
-	return population;
-}
 
-function wrap(n) {
-	if (n < 0) return worldSize - 1;
-	if (n === worldSize) return 0;
-	return n;
-}
+	function wrap(n) {
+		if (n < 0) return worldSize - 1;
+		if (n === worldSize) return 0;
+		return n;
+	}
 
-function isItAlive(population, cell) {
-	if (population === 5) return 1;
-	if (population === 6) return cell;
-	return 0;
-}
+	function isItAlive(population, cell) {
+		if (population === 5) return 1;
+		if (population === 6) return cell;
+		return 0;
+	}
 
-setInterval(function() {
-	cells = nextGen(cells);
-	console.log(nextGen(cells));
-}, 30);
+	return {
+		world: world,
+		nextGen: nextGen,
+		seed: seed
+	};
+})();
+
+
+
+// setInterval(function() {
+// 	cells = nextGen(cells);
+// 	console.log(nextGen(cells));
+// }, 30);
 
 
 
