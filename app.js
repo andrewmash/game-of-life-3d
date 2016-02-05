b4de8f4bddabd421bf6db3045ae605c1a100a574
Samuel Ballan <sgb4622@gmail.com>
2016-02-05 15:35:34 -0500

    setup express and simple routes

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var router = express.Router()
+
+app.use(express.static(__dirname + '/js'));
+app.use(express.static(__dirname + '/view'));
+app.use(express.static(__dirname + '/node_modules'));
+
+router.get('/', function(req, res, next) {
+  res.send('/index.html')
+})
+
+module.exports = app;
+
+app.listen(1337)
