68446d7632244cab98378415a89dd677e8a98f9e
Andrew Ash <andrewash9@gmail.com>
2016-03-09 12:39:55 -0500

    port nonsense

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ app.use(express.static(__dirname + '/js'));
 app.use(express.static(__dirname + '/view'));
 app.use(express.static(__dirname + '/node_modules'));
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('/index.html');
 });
 
 module.exports = app;
 
-app.listen(process.env.PORT, function() {
+app.listen(process.env.PORT, function () {
   console.log("\nListening on a very special port");
 });
